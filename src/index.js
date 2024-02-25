5ee9c449c32278d5f100c854aeb79a5649803e9f
EzhkaPro <gala-slim@yandex.ru>
2024-02-26 00:18:47 +0300

    hw1

--- src/index.js
+++ src/index.js
@@ -9,66 +9,41 @@ const server = http.createServer((request, response) => {
 
   const searchParams = url.searchParams;
 
-  const name = searchParams.get("hello");
-
-  if (searchParams.has("hello")) {
-    if (name === "") {
-      response.statusCode = 400;
-      response.statusMessage = "Error";
-      response.header("Content-Type", "text/plain");
-      response.write("Enter a name");
-      response.end();
-      return;
-    }
-    response.statusCode = 200;
-    response.statusMessage = "OK";
-    response.header("Content-Type", "text/plain");
-    response.write(`Hello,${name}`);
-    response.end();
-    return;
-  }
-
-  if (name) {
-    response.statusCode = 200;
-    response.statusMessage = "ok";
-    response.header = "Content-Type: text/plain";
-    response.write(`Hello, ${name}.`);
-    response.end();
-    return;
-  }
-
-  if (request.url === "/?hello") {
-    response.statusCode = 400;
-    response.statusMessage = "Error";
-    response.header = "Content-Type: text/plain";
-    response.write(`Enter a name`);
-    response.end();
-    return;
-  }
-
-  if (request.url === "/?users") {
-    response.statusCode = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(getUsers());
-    response.end();
-    return;
-  }
-
   if (request.url === "/") {
     response.statusCode = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: text/plain";
     response.write("Hello world!");
     response.end();
-    return;
+   
+  } else if (searchParams.has("hello")) {
+    const name = searchParams.get("hello");
+    if (!name) {
+      response.statusCode = 400;
+      response.statusMessage = "Error";
+      response.header = "Content-Type: text/plain";
+      response.write(`Enter a name`);
+      response.end();
+        } else {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: text/plain";
+      response.write(`Hello, ${name}.`);
+      response.end();
+    }
+  } else if (request.url === "/?users") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+  } else {
+    response.statusCode = 500;
+    response.statusMessage = "Internal Server Error";
+    response.header = "Content-Type: text/plain";
+    response.write("Internal Server Error");
+    response.end();
   }
-
-  response.statusCode = 500;
-  response.statusMessage = "Internal Server Error";
-  response.header = "Content-Type: text/plain";
-  response.write("Internal Server Error");
-  response.end();
 });
 
 server.listen(port, hostname, () => {
